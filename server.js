efa8226694a90aa0fe76c79dd85b3e6f87cbf3d6
Mounika <24wh1a05n0@bvrithyderabad.edu.in>
2026-07-03 12:07:48 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express()
+const port=3002;
+app.use(express.static("frontend"))
+app.listen(port,function(){
+    console.log("successfully running at http://localhost:"+port)
+})
+
